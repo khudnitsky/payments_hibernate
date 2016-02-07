d19cc161d76d90496e9aeaa619c1d8380a0f3f9d
khudnitsky <khudnitsky@gmail.com>
2016-02-07 23:24:40 +0300

    Added ServiceException

--- services/src/main/java/by/pvt/khudnitsky/payments/exceptions/ServiceException.java
+++ services/src/main/java/by/pvt/khudnitsky/payments/exceptions/ServiceException.java
@@ -4,6 +4,12 @@ package by.pvt.khudnitsky.payments.exceptions;
  * Copyright (c) 2016, Khudnitsky. All rights reserved.
  */
 public class ServiceException extends Exception {
+    public ServiceException() {
+        super();
+    }
+
+
+
     public ServiceException(String message){
         super(message);
     }
