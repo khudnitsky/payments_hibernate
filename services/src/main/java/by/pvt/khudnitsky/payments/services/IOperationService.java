2ebc490d8d7f7f473469d0444d2576c6f916d964
khudnitsky <khudnitsky@gmail.com>
2016-02-07 23:25:54 +0300

    Added own interfaces to services

---
+++ services/src/main/java/by/pvt/khudnitsky/payments/services/IOperationService.java
@@ -0,0 +1,11 @@
+package by.pvt.khudnitsky.payments.services;
+
+import by.pvt.khudnitsky.payments.entities.Operation;
+
+/**
+ * Created by: khudnitsky
+ * Date: 07.02.2016
+ * Time: 19:59
+ */
+public interface IOperationService extends IService<Operation> {
+}
